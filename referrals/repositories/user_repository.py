6292330e2eb3908d4c672994d6c350df26dec7da
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-29 23:56:05 +0300

    add UserRepository for database operations on User model

---
+++ referrals/repositories/user_repository.py
@@ -0,0 +1,15 @@
+from referrals.models import User
+
+
+class UserRepository:
+    @staticmethod
+    def get_user_by_phone(phone_number):
+        return User.objects.filter(phone_number=phone_number).first()
+
+    @staticmethod
+    def create_user(phone_number):
+        return User.objects.create(phone_number=phone_number)
+
+    @staticmethod
+    def get_user_by_invite_code(invite_code):
+        return User.objects.filter(invite_code=invite_code).first()
