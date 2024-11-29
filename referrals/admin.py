46f0b15149df93b64a733bcfa1d9056bae999c0f
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-30 00:31:23 +0300

    add authcode to admin, add search_fields, list_filter

--- referrals/admin.py
+++ referrals/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from referrals.models import User
+from referrals.models import AuthCode, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('phone_number', 'invite_code', 'used_invite_code')
+    list_display = ("phone_number", "invite_code", "used_invite_code")
+    search_fields = ("phone_number", "invite_code")
+    list_filter = ("used_invite_code",)
+
+
+@admin.register(AuthCode)
+class AuthCodeAdmin(admin.ModelAdmin):
+    list_display = ("phone_number", "code", "created_at")
+    search_fields = ("phone_number",)
+    list_filter = ("created_at",)
