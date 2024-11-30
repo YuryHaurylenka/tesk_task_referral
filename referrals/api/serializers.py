6f7f1638c32b95e359a0875090af918e6630d2f2
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-30 14:16:05 +0300

    add serializers for invite code activation and code verification

--- referrals/api/serializers.py
+++ referrals/api/serializers.py
@@ -7,3 +7,26 @@ class UserReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["phone_number", "invite_code", "used_invite_code"]
+
+
+class ActivateInviteCodeSerializer(serializers.Serializer):
+    invite_code = serializers.CharField(max_length=6, required=True)
+
+
+class RequestCodeSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=15,
+        required=True,
+        error_messages={"required": "Phone number is required."},
+    )
+
+
+class VerifyCodeSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=15,
+        required=True,
+        error_messages={"required": "Phone number is required."},
+    )
+    code = serializers.CharField(
+        max_length=4, required=True, error_messages={"required": "Code is required."}
+    )
