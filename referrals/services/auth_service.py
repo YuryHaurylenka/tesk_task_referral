d78dab1fb0957073e26985e225c16fab22a51b6c
GavrilenkoYura <gshshaaha@gmail.com>
2024-11-30 14:17:20 +0300

    add delay to auth code request and implement cleaning of expired auth codes

--- referrals/services/auth_service.py
+++ referrals/services/auth_service.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 from referrals.repositories.auth_repository import AuthRepository
@@ -9,18 +10,26 @@ class AuthService:
         self.auth_repository = auth_repository or AuthRepository()
         self.user_service = user_service or UserService()
 
-    def request_auth_code(self, phone_number):
+    def generate_auth_code(self):
+        return f"{randint(1000, 9999)}"
 
-        code = f"{randint(1000, 9999)}"
-        auth_code = self.auth_repository.create_or_update_auth_code(phone_number, code)
-        return auth_code
+    def request_auth_code(self, phone_number):
+        time.sleep(randint(1, 2))
+        code = self.generate_auth_code()
+        return self.auth_repository.create_or_update_auth_code(phone_number, code)
 
     def verify_auth_code(self, phone_number, code):
         auth_code = self.auth_repository.get_auth_code_by_phone(phone_number)
-        if not auth_code or auth_code.code != code or not auth_code.is_valid():
-            return {"success": False, "error": "Invalid or expired code."}
+        if not auth_code or auth_code.code != code:
+            return {"success": False, "error": "Invalid code."}
+        if not auth_code.is_valid():
+            return {"success": False, "error": "Code expired."}
 
         auth_code.delete()
 
         user = self.user_service.register_user(phone_number)
         return {"success": True, "user": user}
+
+    def clean_expired_auth_codes(self):
+        deleted_count = self.auth_repository.clean_expired_auth_codes()
+        return {"success": True, "deleted_count": deleted_count}
