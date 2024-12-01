1ea07857e8daba61b6da10bec2bee824d891fb94
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-01 23:25:17 +0300

    remove pythonanywhere from wsgi

--- referral_test_task/wsgi.py
+++ referral_test_task/wsgi.py
@@ -1,14 +1,7 @@
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-project_home = "/home/yurahaurylenka/test_task_referral"
-if project_home not in sys.path:
-    sys.path.append(project_home)
-
-activate_this = "/home/your-username/your-project/venv/bin/activate_this.py"
-exec(open(activate_this).read(), dict(__file__=activate_this))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "referral_test_task.settings")
 
 application = get_wsgi_application()
