b84b2b80be78bd2e4df44abc3b600f56a84a9332
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-02 02:30:41 +0300

    refactor database url formatting in settings

--- referral_test_task/settings.py
+++ referral_test_task/settings.py
@@ -106,7 +106,9 @@ WSGI_APPLICATION = "referral_test_task.wsgi.application"
 
 # Database configuration using PostgreSQL with environment variables
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+    )
 }  # Django REST Framework configuration
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
