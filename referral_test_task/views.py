1e713f893ba444afa92a3a9613426734373ad97e
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-02 02:52:38 +0300

    add exception to custom404(with it does not work)

--- referral_test_task/views.py
+++ referral_test_task/views.py
@@ -8,5 +8,5 @@ def home_view(request):
         return redirect("request_code")
 
 
-def custom_404(request):
+def custom_404(request, exception=None):
     return render(request, "404.html", status=404)
