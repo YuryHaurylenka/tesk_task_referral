82c93a1ec7f98b9be86dc4ff71c23f3ddd340afb
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-01 19:34:02 +0300

    add redirect to profile or request_code

--- referral_test_task/views.py
+++ referral_test_task/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def home_view(request):
-    return render(request, "base.html")
+    if request.session.get("access_token"):
+        return redirect("profile")
+    else:
+        return redirect("request_code")
