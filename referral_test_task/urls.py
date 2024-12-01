fc6056bd8daac09b34765f49eaf1e83c766d2e2e
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-02 02:25:51 +0300

    refactor

--- referral_test_task/urls.py
+++ referral_test_task/urls.py
@@ -5,11 +5,13 @@ from django.urls import include, path
 from referral_test_task.views import home_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls) if settings.DEBUG else None,
     path("api/", include("referrals.api.urls")),
     path("", home_view, name="home"),
     path("", include("referrals.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path("admin/", admin.site.urls))
 
 if settings.DEBUG:
     import debug_toolbar
