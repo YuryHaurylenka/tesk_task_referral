e65fb219acfa9e1fe76af523ab690d85801c4fc9
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-01 23:14:40 +0300

    add static root

--- referral_test_task/settings.py
+++ referral_test_task/settings.py
@@ -166,6 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
