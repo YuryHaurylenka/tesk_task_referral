fac3e2d24f4b19fb640158bddd2d38ac0241dbff
GavrilenkoYura <gshshaaha@gmail.com>
2024-12-01 18:23:31 +0300

    simplify URL routing and remove token paths

--- referrals/api/urls.py
+++ referrals/api/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from referrals.api.views import AuthViewSet, UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-router.register(r'auth', AuthViewSet, basename='auth')
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"auth", AuthViewSet, basename="auth")
 
-urlpatterns = [
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-] + router.urls
+urlpatterns = router.urls
